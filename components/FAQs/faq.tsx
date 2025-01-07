6b0a46a58db82ae1203598d401f72a44da5be83e
tochukwunwosa <tochukwunwosa28@gmail.com>
2025-01-07 08:02:12 +0100

    feat: Implement FAQ page with search functionality and layout; add metadata for SEO

--- components/FAQs/faq.tsx
+++ components/FAQs/faq.tsx
@@ -22,19 +22,19 @@ export default function FAQWrapper({
           <div className="flex items-center ">
             {/* icon left */}
             <Link2 />
-            <div className="flex w-full items-center justify-between rounded-md bg-muted px-4  text-left font-medium transition-colors hover:bg-muted/50 focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-opacity-75">
-              <p className="text-[#000929] font-[500] md:text-[1.5rem]">
+            <div className="flex w-full items-center justify-between rounded-md bg-muted px-4 py-2 lg:py-5  text-left font-medium transition-colors hover:bg-muted/50 focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-opacity-75">
+              <p className="text-[#000929] font-[500] text-base md:text-[1.25rem] lg:text-[1.5rem] w-full">
                 {question}
               </p>
               <div
                 onClick={() => setIsOpenAnser(!isOpenAnswer)}
-                className="cursor-pointer"
+                className="cursor-pointer w-fit"
               >
                 {/* icon right */}
                 <Image
                   src={isOpenAnswer ? '/chevronOpen.png' : '/chevronClose.png'}
-                  height={28}
-                  width={28}
+                  height={18}
+                  width={18}
                   alt="chevron"
                 />
               </div>
@@ -42,7 +42,7 @@ export default function FAQWrapper({
           </div>
           {isOpenAnswer && (
             <div className="pl-10 pr-4 pb-4 text-muted-foreground">
-              <p className="text-[#808080] md:text-[1.25rem] font-normal">
+              <p className="text-[#808080] text-sm md:text-base lg:text-[1.25rem] font-normal">
                 {answer}
               </p>
             </div>
--- config/FAQs.ts
+++ config/FAQs.ts
@@ -1,9 +1,9 @@
-interface FAQsContentTypes {
+interface FAQType {
   question: string;
   answer: string;
 }
 
-export const FAQsContent: FAQsContentTypes[] = [
+export const FAQsContent: FAQType[] = [
   {
     question: 'Who is EzyRent?',
     answer:
@@ -18,5 +18,53 @@ export const FAQsContent: FAQsContentTypes[] = [
     question: 'How does one earn as a property manager?',
     answer:
       'You earn by seamlessly renting your property out to potential tenants.'
+  }
+];
+
+export const FAQFullData: FAQType[] = [
+  
+  {
+    question: "When is the best period to rent a house in Abuja?",
+    answer: "The best period depends on your personal needs and market conditions, but typically, off-peak seasons can offer more negotiation opportunities."
+  },
+  {
+    question: "What is EzyRent?",
+    answer: "EzyRent is a digital rental solution designed to streamline the rental process in Nigeria, by eradicating high rates and scams."
+  },
+  {
+    question: "Who is this application for?",
+    answer: "EzyRent is designed for anyone involved in the rental process in Nigeria, aiming to make renting seamless while eliminating scams and high costs."
+  },
+  {
+    question: "How does one earn as a property manager?",
+    answer: "Property managers earn commissions from the profit made on rented apartments and can also earn by conducting house tours for tenants to ensure the property is in good condition."
+  },
+  {
+    question: "What are the benefits of joining EzyRent?",
+    answer: "Key benefits include access to a curated selection of rental properties tailored to diverse needs and budgets."
+  },
+  {
+    question: "What makes EzyRent different?",
+    answer: "Verified Listings: Every property is inspected and verified for authenticity. Subscription Plan: Flexible options for both tenants and landlords. Secure Transaction: Payments handled via escrow. Community Support: Real-time assistance from a network of property managers."
+  },
+  {
+    question: "When will the app launch?",
+    answer: "The EzyRent application is scheduled for release in the 3rd week of January 2025, subject to testing outcomes."
+  },
+  {
+    question: "Can I reserve a property without paying the full rent upfront?",
+    answer: "This feature is exclusive to premium subscribers, allowing them to reserve properties without paying the full rent upfront."
+  },
+  {
+    question: "How do I become a property manager?",
+    answer: "Sign up on EzyRent as a property manager, set up your profile, and pass the verification process to start earning by accepting inspection offers."
+  },
+  {
+    question: "How does the escrow system work?",
+    answer: "Rent payments are securely held in escrow until you confirm the property and terms. Once satisfied, the funds are released to the landlord."
+  },
+  {
+    question: "How do I benefit as a tenant?",
+    answer: "Tenants can search for verified properties across Nigeria, book property inspections via the app, make secure payments through the escrow system, and get assistance from local property managers."
   }
 ];
