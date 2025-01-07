b37c8f6c0795d1a43c1bfcae38b75d0c8873b499
tochukwunwosa <tochukwunwosa28@gmail.com>
2025-01-07 16:50:02 +0100

    refactor: Clean up code formatting and improve consistency across various files

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,36 +13,63 @@ export default function About() {
           {/* Hero Section */}
           <header className="mt-5 relative max-w-[1240px] h-[400px] md:h-[500px] rounded-[40px] bg-[url('/about/about-image2479x1653.jpeg')] bg-no-repeat bg-center bg-cover overflow-hidden">
             <div className="absolute inset-0 bg-[#000929] bg-opacity-55 flex flex-col justify-center items-center text-white text-center h-full">
-              <div className='max-w-[900px] mx-auto flex flex-col items-center justify-center h-full'>
-                <h1 className="mb-10 sm:text-[1.2rem] text-[1.5rem] md:text-[3rem] lg:text-[3.5rem] font-semibold !leading-[1.4]">Transforming Rentals, <br />
-                  One Listing at a Time</h1>
-                <em className="text-xs md:text-xl lg:text-[1.5rem] mt-2 !leading-[1.5]"><span className="bg-white rounded-[40px] font-extrabold py-1 px-2 lg:py-2 lg:px-4 text-[#000929]">EzyRent</span> is revolutionizing property rentals across Nigeria and Africa. Making it seamless to find, rent or sell homes from anywhere in the world. With user-first approach, we eliminate the hassle of agent fees and bring transparency , convenience, and security to the rental process - all from the comfort of your home.</em>
+              <div className="max-w-[900px] mx-auto flex flex-col items-center justify-center h-full">
+                <h1 className="mb-10 sm:text-[1.2rem] text-[1.5rem] md:text-[3rem] lg:text-[3.5rem] font-semibold !leading-[1.4]">
+                  Transforming Rentals, <br />
+                  One Listing at a Time
+                </h1>
+                <em className="text-xs md:text-xl lg:text-[1.5rem] mt-2 !leading-[1.5]">
+                  <span className="bg-white rounded-[40px] font-extrabold py-1 px-2 lg:py-2 lg:px-4 text-[#000929]">
+                    EzyRent
+                  </span>{' '}
+                  is revolutionizing property rentals across Nigeria and Africa.
+                  Making it seamless to find, rent or sell homes from anywhere
+                  in the world. With user-first approach, we eliminate the
+                  hassle of agent fees and bring transparency , convenience, and
+                  security to the rental process - all from the comfort of your
+                  home.
+                </em>
               </div>
             </div>
           </header>
 
           {/* Mission Section */}
           <section className="py-10 lg:py-20 max-w-[900px] mx-auto">
-            <h2 className="lg:mb-10 text-2xl md:text-3xl lg:text-[2.5rem] 2xl:text-[3rem] font-medium text-[#7065F0] text-center">Our Mission</h2>
+            <h2 className="lg:mb-10 text-2xl md:text-3xl lg:text-[2.5rem] 2xl:text-[3rem] font-medium text-[#7065F0] text-center">
+              Our Mission
+            </h2>
             <p className="max-w-[800px] mx-auto mt-2 lg:mt-4 lg:text-center lg:text-[1.1rem] text-gray-600">
-              At <span className="font-extrabold">EzyRent</span>, our mission is to revolutionize the property rental experience by creating a seamless and trusted platform that bridges the gap between renters, landlords, and property managers.
-              We empower renters with broader, reliable housing options and easy access to secure, affordable homes. We support landlords by providing them with an extensive pool of verified tenants and the tools to manage their properties efficiently.
+              At <span className="font-extrabold">EzyRent</span>, our mission is
+              to revolutionize the property rental experience by creating a
+              seamless and trusted platform that bridges the gap between
+              renters, landlords, and property managers. We empower renters with
+              broader, reliable housing options and easy access to secure,
+              affordable homes. We support landlords by providing them with an
+              extensive pool of verified tenants and the tools to manage their
+              properties efficiently.
             </p>
           </section>
 
           {/* What We Offer Section */}
           <section className="py-10 max-w-[900px] mx-auto ">
-            <h2 className="text-2xl md:text-3xl lg:text-[2.5rem] 2xl:text-[3rem] font-medium text-[#7065F0] text-center mb-2 lg:mb-4">What We Offer</h2>
+            <h2 className="text-2xl md:text-3xl lg:text-[2.5rem] 2xl:text-[3rem] font-medium text-[#7065F0] text-center mb-2 lg:mb-4">
+              What We Offer
+            </h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-[1240px]  mx-auto px-4">
               {serviceFeatures.map((service, index) => (
-                <div key={service.title + index} className="max-w-[260px] mx-auto rounded-[40px] border-[2px] border-[#000929] border-opacity-15 flex flex-col items-center space-y-4 p-6 bg-white shadow text-center">
+                <div
+                  key={service.title + index}
+                  className="max-w-[260px] mx-auto rounded-[40px] border-[2px] border-[#000929] border-opacity-15 flex flex-col items-center space-y-4 p-6 bg-white shadow text-center"
+                >
                   <div className="border-[2px] border-black border-opacity-20 w-fit p-2 rounded-full flex item-center justify-center">
                     <service.icon size={20} />
                   </div>
-                  <h3 className="text-base md:text-lg lg:text-2xl font-medium text-[#000929] text-center">{service.title}</h3>
-                  <p className="font-light text-sm md:text-lg text-center text-[#000929] text-opacity-50">{
-                    service.description}</p>
-
+                  <h3 className="text-base md:text-lg lg:text-2xl font-medium text-[#000929] text-center">
+                    {service.title}
+                  </h3>
+                  <p className="font-light text-sm md:text-lg text-center text-[#000929] text-opacity-50">
+                    {service.description}
+                  </p>
                 </div>
               ))}
             </div>
@@ -50,7 +77,9 @@ export default function About() {
 
           {/* Team Members Section */}
           <section className="py-10 max-w-[1240px] mx-auto ">
-            <h2 className="!mb-10 text-2xl md:text-3xl lg:text-[2.5rem] 2xl:text-[3rem] font-medium text-[#7065F0] text-center">Meet the Team</h2>
+            <h2 className="!mb-10 text-2xl md:text-3xl lg:text-[2.5rem] 2xl:text-[3rem] font-medium text-[#7065F0] text-center">
+              Meet the Team
+            </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full mx-auto mt-2 lg:mt-4">
               {teamMembers.map((member, index) => (
                 <div
@@ -67,7 +96,6 @@ export default function About() {
                         height={180}
                         className="w-full h-full rounded-full object-top object-cover"
                       />
-
                     </div>
                   </div>
 
@@ -88,7 +116,6 @@ export default function About() {
                 </div>
               ))}
             </div>
-
           </section>
         </main>
       </MaxWidthWrapper>
--- config/about.ts
+++ config/about.ts
@@ -1,4 +1,4 @@
-import { House, LucideIcon, Star, Wallet } from "lucide-react";
+import { House, LucideIcon, Star, Wallet } from 'lucide-react';
 
 interface ServiceFeature {
   icon: LucideIcon;
@@ -27,44 +27,47 @@ export const serviceFeatures: ServiceFeature[] = [
   }
 ];
 
-
-
-
 export const teamMembers: TeamMember[] = [
   {
-    image:"femi-david.jpeg",
+    image: 'femi-david.jpeg',
     name: 'Femi David',
     role: 'CEO & Founder',
-    description: 'David is a visionary entrepreneur, with a wealth of experience in business and a passion for transforming spaces. Femi has successfully spearheaded multiple high-impact projects.'
+    description:
+      'David is a visionary entrepreneur, with a wealth of experience in business and a passion for transforming spaces. Femi has successfully spearheaded multiple high-impact projects.'
   },
   {
-    image:"adebayo-adekunle.jpeg",
+    image: 'adebayo-adekunle.jpeg',
     name: 'Adebayo Adekunle',
     role: 'Chief Operating Officer (COO)',
-    description: 'Adebayo is a seasoned operations expert with over a decade of experience in streamlining processes and enhancing efficiency in real estate.',
+    description:
+      'Adebayo is a seasoned operations expert with over a decade of experience in streamlining processes and enhancing efficiency in real estate.'
   },
   {
-    image:"chinelo-okafor.jpeg",
+    image: 'chinelo-okafor.jpeg',
     name: 'Chinelo Okafor',
     role: 'Chief Financial Officer (CFO)',
-    description: 'Chinelo brings a wealth of financial acumen to the team, with a solid background in corporate finance and investment management. She manages the company’s budgets, & forecasts.'
+    description:
+      'Chinelo brings a wealth of financial acumen to the team, with a solid background in corporate finance and investment management. She manages the company’s budgets, & forecasts.'
   },
   {
-    image: "tunde-ajayi.jpeg",
+    image: 'tunde-ajayi.jpeg',
     name: 'Tunde Ajayi',
     role: 'Head of Marketing and Sales',
-    description: 'Tunde is a dynamic marketer with a flair for connecting clients to their dream properties. With his in-depth understanding of market trends and customer behavior.'
+    description:
+      'Tunde is a dynamic marketer with a flair for connecting clients to their dream properties. With his in-depth understanding of market trends and customer behavior.'
   },
   {
-    image: "chinelo-okafor.jpeg",
+    image: 'chinelo-okafor.jpeg',
     name: 'Fatimah Abdullahi',
     role: 'Legal and Compliance Manager',
-    description: 'Fatimah is a skilled legal professional specializing in property law and compliance. She ensures that all transactions and projects adhere to regulatory requirements.'
+    description:
+      'Fatimah is a skilled legal professional specializing in property law and compliance. She ensures that all transactions and projects adhere to regulatory requirements.'
   },
   {
-    image: "femi-david.jpeg",
+    image: 'femi-david.jpeg',
     name: 'Obinna Nwosu',
     role: 'Project Manager',
-    description: 'Obinna is a certified project manager with a reputation for delivering complex real estate developments. He oversees planning, execution, and quality control across all company projects.'
+    description:
+      'Obinna is a certified project manager with a reputation for delivering complex real estate developments. He oversees planning, execution, and quality control across all company projects.'
   }
 ];
