915c0106fddf16b61ccbb9d12ab5bc3e3ea60731
tochukwunwosa <tochukwunwosa28@gmail.com>
2025-01-15 12:42:05 +0100

    style: Property listing made responsive

--- app/property-listing/page.tsx
+++ app/property-listing/page.tsx
@@ -28,107 +28,116 @@ export default function PropertyListing() {
       <MaxWidthWrapper>
         <Breadcrumb />
         <main className="mt-10">
-          <section className="flex item-center space-x-5">
+          <section className="flex flex-col space-y-5 lg:flex-row lg:space-y-0 item-center lg:space-x-5 ">
             <div className="relative w-full">
               <Search className="absolute left-2 top-[30%] " />
               <Input placeholder="Search by address" className="pl-10" />
             </div>
-            {/* type */}
-            <div>
-              <Select>
-                <SelectTrigger className="flex items-center w-[100px]">
-                  <SelectValue placeholder="Select type" />
-                </SelectTrigger>
+            {/* type and min */}
+            <div className="grid grid-col-1 sm:grid-cols-2 gap-5 w-full">
+              {/* type */}
+              <div>
+                <Select>
+                  <SelectTrigger className="flex items-center ">
+                    <SelectValue placeholder="Select type" />
+                  </SelectTrigger>
 
-                <SelectContent className="bg-white">
-                  {propertyType.map((type: string) => (
-                    <SelectItem key={type} value={type} className="capitalize">
-                      {type}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-            {/* min price */}
-            <div>
-              <Select>
-                <SelectTrigger className="flex items-center w-[100px]">
-                  <SelectValue placeholder="Min price" />
-                </SelectTrigger>
+                  <SelectContent className="bg-white">
+                    {propertyType.map((type: string) => (
+                      <SelectItem key={type} value={type} className="capitalize">
+                        {type}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              {/* min price */}
+              <div>
+                <Select>
+                  <SelectTrigger className="flex items-center ">
+                    <SelectValue placeholder="Min price" />
+                  </SelectTrigger>
 
-                <SelectContent className="bg-white">
-                  {minPropertyPrice.map((minPrice: string) => (
-                    <SelectItem
-                      key={minPrice}
-                      value={minPrice}
-                      className="capitalize"
-                    >
-                      {minPrice}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+                  <SelectContent className="bg-white">
+                    {minPropertyPrice.map((minPrice: string) => (
+                      <SelectItem
+                        key={minPrice}
+                        value={minPrice}
+                        className="capitalize"
+                      >
+                        {minPrice}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
             </div>
-            {/* max price */}
-            <div>
-              <Select>
-                <SelectTrigger className="flex items-center w-[100px]">
-                  <SelectValue placeholder="Max price" />
-                </SelectTrigger>
+            {/* max and beds */}
+            <div className="grid grid-col-1 md:grid-cols-2 gap-5 w-full">
+              {/* max price */}
+              <div>
+                <Select>
+                  <SelectTrigger className="flex items-center ">
+                    <SelectValue placeholder="Max price" />
+                  </SelectTrigger>
 
-                <SelectContent className="bg-white">
-                  {maxPropertyPrice.map((maxPrice: string) => (
-                    <SelectItem
-                      key={maxPrice}
-                      value={maxPrice}
-                      className="capitalize"
-                    >
-                      {maxPrice}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-            {/* beds */}
-            <div>
-              <Select>
-                <SelectTrigger className="flex items-center w-[100px]">
-                  <SelectValue placeholder="Bed" />
-                </SelectTrigger>
+                  <SelectContent className="bg-white">
+                    {maxPropertyPrice.map((maxPrice: string) => (
+                      <SelectItem
+                        key={maxPrice}
+                        value={maxPrice}
+                        className="capitalize"
+                      >
+                        {maxPrice}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              {/* beds */}
+              <div>
+                <Select>
+                  <SelectTrigger className="flex items-center ">
+                    <SelectValue placeholder="Bed" />
+                  </SelectTrigger>
 
-                <SelectContent className="bg-white">
-                  {propertyBeds.map((bed: string) => (
-                    <SelectItem key={bed} value={bed} className="capitalize">
-                      {bed}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+                  <SelectContent className="bg-white">
+                    {propertyBeds.map((bed: string) => (
+                      <SelectItem key={bed} value={bed} className="capitalize">
+                        {bed}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
             </div>
-            {/* more filters */}
-            <div>
-              <Select>
-                <SelectTrigger className="flex items-center w-[100px] ">
-                  <SlidersHorizontal size={12} className="mr-2" />
-                  <SelectValue placeholder="filter more" />
-                </SelectTrigger>
+            {/* fliter and search */}
+            <div className="grid grid-col-1 md:grid-cols-2 gap-5 w-full">
+              {/* more filters */}
+              <div>
+                <Select>
+                  <SelectTrigger className="flex items-center  ">
+                    <SlidersHorizontal size={12} className="mr-2" />
+                    <SelectValue placeholder="filter more" />
+                  </SelectTrigger>
 
-                <SelectContent className="bg-white">
-                  {extraFilters.map((filter: string) => (
-                    <SelectItem
-                      key={filter}
-                      value={filter}
-                      className="capitalize"
-                    >
-                      {filter}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-            {/* search btn */}
-            <div>
-              <Button className="bg-[#7065F0]">Search</Button>
+                  <SelectContent className="bg-white">
+                    {extraFilters.map((filter: string) => (
+                      <SelectItem
+                        key={filter}
+                        value={filter}
+                        className="capitalize"
+                      >
+                        {filter}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              {/* search btn */}
+              <div>
+                <Button className="bg-[#7065F0]">Search</Button>
+              </div>
             </div>
           </section>
         </main>
