e4931071e9b53188fbeb6def9d7d8025cabecad4
Igashi Michael <mikkybeardless@gmail.com>
2025-01-16 16:01:05 +0100

    add get atrted section

---
+++ app/download/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MaxWidthWrapper from '../maxWidthWrapper';
+import Breadcrumb from '@/components/breadcrumb';
+
+export default function Download() {
+  return (
+    <section>
+        <MaxWidthWrapper>
+            <Breadcrumb />
+            Download Page
+        </MaxWidthWrapper>
+    </section>
+  );
+}
