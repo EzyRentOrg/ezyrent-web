cf42f85f718b188f41eff2477156bca957038ed9
Igashi <mikkybeardless@gmail.com>
2025-03-28 15:54:43 +0100

    add metadata to website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,41 @@ import { dmSans } from '@/lib/font';
 import { cn } from '@/lib/utils';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Toaster } from 'sonner';
-import { generateMetadata } from '@/lib/metadata';
+// import { generateMetadata } from '@/lib/metadata';
 import './globals.css';
+import { Metadata } from 'next';
 
-export const metadata = generateMetadata({
-  title: 'Home',
-  description: 'Welcome to EzyRent',
-  path: ''
-});
+export const metadata: Metadata = {
+  title: 'EzyRent - Rent Smarter, Live Easier',
+  description:
+    'Find and rent your dream home easily with EzyRent. No stress, no hidden fees!',
+  openGraph: {
+    title: 'EzyRent - Rent Smarter, Live Easier',
+    description:
+      'Find and rent your dream home easily with EzyRent. No stress, no hidden fees!',
+    images: [
+      {
+        url: 'https://www.ezyrent.org/images/preview.jpg',
+        width: 1200,
+        height: 630
+      }
+    ],
+    url: 'https://www.ezyrent.org',
+    type: 'website'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'EzyRent - Rent Smarter, Live Easier',
+    description:
+      'Find and rent your dream home easily with EzyRent. No stress, no hidden fees!',
+    images: ['https://www.ezyrent.org/images/preview.jpg']
+  }
+};
+// export const metadata = generateMetadata({
+//   title: 'Home',
+//   description: 'Welcome to EzyRent',
+//   path: ''
+// });
 
 export default function RootLayout({
   children
