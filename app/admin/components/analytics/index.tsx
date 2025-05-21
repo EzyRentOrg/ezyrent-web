ff2abff47b024ddddfea75b6887719d6707b262c
Igashi <mikkybeardless@gmail.com>
2025-05-21 16:39:22 +0100

    fixed analytics mobile view

--- app/admin/components/analytics/index.tsx
+++ app/admin/components/analytics/index.tsx
@@ -17,7 +17,7 @@ export default function Analytics() {
   return (
     <div className="flex flex-col gap-5 px-4 md:px-10 py-5">
       {/* statistics section */}
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {DashboardStats?.map((stat, index) => (
           <StatsCard
             key={index}
