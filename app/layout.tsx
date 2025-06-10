1be76d6f9789b91a4a57afb6bc5af723257fcd1e
Tochukwu Nwosa <tochukwunwosa28@gmail.com>
2025-06-10 16:28:57 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
+          id="EzyRent description"
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
